f27e8e468f0058775352f0028d2f07b354638ba7
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2022-11-28 22:54:27 -0500

    Add functionality to buttons to enable mouseover once selected

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const gridContainer = document.getElementById('gridContainer');
-
+const allButtons = document.querySelectorAll('button');
+let colorMode;
 
 function createGrid(column, row){
     for(let i = 0; i < column; i++){
@@ -12,10 +13,81 @@ function createGrid(column, row){
             column.appendChild(row);
         }
     }
+}    
+
+createGrid(16,16);
+
+
+
+
+
+const cells = gridContainer.querySelectorAll('.row');
+
+allButtons.forEach(button =>{
+    button.addEventListener('click', (e)=>{
+        colorMode = e.currentTarget.id;
+        console.log(colorMode);
+        cells.forEach(addMouseOver);
+        
+    });
+})
+
+function addMouseOver(cell){
+    cell.addEventListener('mouseover', buttonChoice);
+
+}
+
+function buttonChoice(){
+    if(colorMode === "black"){
+        this.style.backgroundColor = 'black';
+    }
+    if(colorMode === "darken"){
+        this.style.backgroundColor = "black";
+        this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.2;
+    }
+    if(colorMode === "eraser"){
+        this.style.backgroundColor = "white";
+    }
 }
 
 
 
-createGrid(2,2);
 
+
+
+
+// console.log(cells);
+
+// cells.forEach(cell =>{
+//     cell.addEventListener('mouseover', (e)=>{
+//         setColorBlack(e);
+        
+//     })
+// })
+
+
+
+
+
+
+
+
+
+// function setToEraser(event){
+//     event.currentTarget.style.backgroundColor="white";
+// }
+
+
+
+ 
+
+
+
+// cells.forEach((cell)=>{
+//     cell.addEventListener('mouseover', setColorBlack);
+// });
+
+// // function setColorBlack(){
+// //     this.style.backgroundColor = "black";
+// // }
 
